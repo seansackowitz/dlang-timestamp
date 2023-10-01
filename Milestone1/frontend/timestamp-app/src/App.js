2a7329a5c5b0b8d0a7b53566611f32e3304d5d04
dhlang <dhlang@ncsu.edu>
2023-09-30 21:41:35 -0400

    Making initial push for the project, which includes the tailwind.config.js and the initial login page

--- Milestone1/frontend/timestamp-app/src/App.js
+++ Milestone1/frontend/timestamp-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="./page/login.html"
           target="_blank"
           rel="noopener noreferrer"
         >
