7a8e7e76dc1489dc45628e94750ca7b9625f44c4
kmai <kmai@ncsu.edu>
2023-10-02 15:54:42 -0400

    added login.jsx

--- Milestone1/frontend/timestamp-app/src/App.js
+++ Milestone1/frontend/timestamp-app/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './page/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="./page/login.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginPage></LoginPage>
     </div>
   );
 }
---
+++ Milestone1/frontend/timestamp-app/src/page/login.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div className=' flex flex-col justify-center h-full items-center'>
+            <h1 className=' text-5xl mb-4'>Login</h1>
+            <div class="w-full max-w-xs border-0">
+                <form class="bg-white rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <label
+                            class="block text-gray-700 text-sm font-bold mb-2"
+                            for="username"
+                        >
+                            Username
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            type="text"
+                            placeholder="Username"
+                        />
+                    </div>
+                    <div class="mb-6">
+                        <label
+                            class="block text-gray-700 text-sm font-bold mb-2"
+                            for="password"
+                        >
+                            Password
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <button
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="button"
+                        >
+                            Sign In
+                        </button>
+                        <a
+                            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                            href="#"
+                        >
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
