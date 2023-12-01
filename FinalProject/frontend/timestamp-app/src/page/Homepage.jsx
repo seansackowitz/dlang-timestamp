72ec0c2cdc2b6eb2e53ed5da68cac5e23d0f447c
Dylan Lang <dhlang@ncsu.edu>
2023-11-30 21:50:22 -0500

    Put the clock in button back

--- FinalProject/frontend/timestamp-app/src/page/Homepage.jsx
+++ FinalProject/frontend/timestamp-app/src/page/Homepage.jsx
@@ -237,12 +237,12 @@ const Homepage = () => {
                     <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
                 </svg>
             </button> */}
-            {/* <button
+            <button
                 className=" middle none center w-60 h-60 mt-10 rounded-full bg-slate-500 py-3.5 px-7 font-sans text-4xl font-bold uppercase text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 data-ripple-light="true"
             >
                 CLOCK IN
-            </button> */}
+            </button>
 
             <Modal open={open} onClose={() => setOpen(false)}>
                 {openCalculatedModal ? (
